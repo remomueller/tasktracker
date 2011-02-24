8fe80df1cf9b818e880ed1385d3f24b25ad3d488
Remo Mueller <remo.mueller@case.edu>
2011-02-24 09:52:17 -0500

    Styling Update for Notes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def cancel
+    link_to image_tag('icons/cross.png', :alt => '') + 'cancel', URI.parse(request.referer.to_s).path.blank? ? root_path : (URI.parse(request.referer.to_s).path), :class => 'button negative'
+  end
+
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,29 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+/* Mouse Out Functions to Show and Hide Divs */
+
+function isTrueMouseOut(e, handler) {
+	if (e.type != 'mouseout') return false;
+	var relTarget;
+  if (e.relatedTarget) {
+    relTarget = e.relatedTarget;
+  } else if (e.type == 'mouseout') {
+    relTarget = e.toElement;
+  } else {
+    relTarget = e.fromElement;
+  }
+// alert('relTarget type:' + relTarget.tagName + " - " + relTarget.innerHTML);
+  while (relTarget && relTarget != handler) {
+    relTarget = relTarget.parentNode;
+  }
+	return (relTarget != handler);
+}
+
+function hideOnMouseOut(element){
+  var element = $(element);
+  element.onmouseout = function(e, handler) {
+    if (isTrueMouseOut(e||window.event, this)) this.hide();
+  }
+}
