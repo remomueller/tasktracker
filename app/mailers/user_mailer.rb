5af654f5f36938e1a99321442ba340934d6ea834
Remo Mueller <remo.mueller@case.edu>
2011-03-22 11:31:31 -0400

    Add footer to notes emails.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -49,5 +49,7 @@ class UserMailer < ActionMailer::Base
   
   def setup_email
     @subject = "[#{DEFAULT_APP_NAME.downcase}#{'-development' if Rails.env == 'development'}] "
+    @footer_html = "Change email settings here: <a href=\"#{SITE_URL}/settings\">#{SITE_URL}/settings</a>.".html_safe
+    @footer_txt = "Change email settings here: #{SITE_URL}/settings."
   end
 end
