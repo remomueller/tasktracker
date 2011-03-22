b6ac23a3b677f2f1242e633e46c479264919f74b
Remo Mueller <remo.mueller@case.edu>
2011-03-22 12:38:27 -0400

    Don't check system admin on settings and update settings.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!, :except => [:new, :create]
-  before_filter :check_system_admin, :except => [:new, :create, :filtered, :index, :show]
+  before_filter :check_system_admin, :except => [:new, :create, :filtered, :index, :show, :settings, :update_settings]
 
   # # Retrieves filtered list of users.
   # def filtered
