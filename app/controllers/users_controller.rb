5d9e229c64a9eaf86d106e776a0c3dd804167990
Remo Mueller <remosm@gmail.com>
2011-04-28 11:10:30 -0400

    Allow System Admins to add new users in the future.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!, :except => [:new, :create]
-  before_filter :check_system_admin, :except => [:new, :create, :filtered, :index, :show, :settings, :update_settings]
+  before_filter :authenticate_user!
+  before_filter :check_system_admin, :only => [:new, :create, :edit, :update, :destroy]
 
-  def settings
-  end
-  
   def update_settings
     notifications = {}
     email_settings = ['send_email', 'sticky_creation', 'project_comments', 'sticky_comments'] + current_user.all_viewable_projects.collect{|p| "project_#{p.id}"}
@@ -29,35 +26,16 @@ class UsersController < ApplicationController
     @user = User.find_by_id(params[:id])
   end
   
-  # # GET /users/new
-  # # GET /users/new.xml
-  # def new
-  #   @user = User.new
-  # 
-  #   respond_to do |format|
-  #     format.html # new.html.erb
-  #     format.xml  { render :xml => @user }
-  #   end
-  # end
+  def new
+    @user = User.new
+  end
 
   def edit
     @user = User.find_by_id(params[:id])
   end
-  
-  # # POST /users
-  # # POST /users.xml
+
+  # # This is in registrations_controller.rb
   # def create
-  #   @user = User.new(params[:user])
-  # 
-  #   respond_to do |format|
-  #     if @user.save
-  #       format.html { redirect_to(@user, :notice => 'User was successfully created.') }
-  #       format.xml  { render :xml => @user, :status => :created, :location => @user }
-  #     else
-  #       format.html { render :action => "new" }
-  #       format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-  #     end
-  #   end
   # end
 
   def update
