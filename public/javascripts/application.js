8fe86520648cbe979f6aa8bdd4a885480003c1cf
Remo Mueller <remosm@gmail.com>
2011-04-28 14:29:55 -0400

    Add more colors

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -127,7 +127,8 @@ function drawHighChartHistogramChart(element_id, values, params, categories){
     	'#DB843D', 
     	'#92A8CD', 
     	'#A47D7C',
-    	'#B5CA92'
+    	'#B5CA92',
+    	'#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'
     ]
   });
   
