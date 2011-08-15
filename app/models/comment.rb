7811f360a888908617a2d3685bb9f89609f60a41
Remo Mueller <remosm@gmail.com>
2011-08-15 12:44:12 -0400

    active? updated to active_for_authentication?

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -48,7 +48,7 @@ class Comment < ActiveRecord::Base
     @object = self.object_model.constantize.find_by_id(self.object_id)
     all_users = (@object.comments.collect{|c| c.user} + [@object.user, @object.owner]).compact.uniq - [self.user]
     all_users.each do |user_to_email|
-      if user_to_email.active? and user_to_email.email_on?(:send_email) and
+      if user_to_email.active_for_authentication? and user_to_email.email_on?(:send_email) and
         ((self.object_model == 'Project' and user_to_email.email_on?(:project_comments) and user_to_email.email_on?("project_#{self.object_id}")) or
         (self.object_model == 'Sticky' and user_to_email.email_on?(:sticky_comments) and user_to_email.email_on?("project_#{Sticky.find_by_id(self.object_id).project.id}")))
         UserMailer.comment_by_mail(self, @object, user_to_email).deliver if Rails.env.production?
