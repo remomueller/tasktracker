a0db6cde153958ac0f3460c03442eb8940610bb2
Remo Mueller <remosm@gmail.com>
2011-12-14 10:31:18 -0500

    :id is no longer required for menu items in Contour 0.7.0

--- config/initializers/contour.rb
+++ config/initializers/contour.rb
@@ -19,11 +19,11 @@ Contour.setup do |config|
   # Enter the items you wish to see in the menu
   config.menu_items = [
     {
-      name: 'Login', id: 'auth', display: 'not_signed_in', path: 'new_user_session_path', position: 'right',
+      name: 'Login', display: 'not_signed_in', path: 'new_user_session_path', position: 'right',
       links: [{ name: 'Sign Up', path: 'new_user_registration_path' }]
     },
     {
-      name: 'current_user.name', eval: true, id: 'auth', display: 'signed_in', path: 'user_path(current_user)', position: 'right',
+      name: 'current_user.name', eval: true, display: 'signed_in', path: 'user_path(current_user)', position: 'right',
       links: [{ html: '"<div class=\"small\" style=\"color:#bbb\">"+current_user.email+"</div>"', eval: true },
               { name: 'Settings', path: 'settings_path' },
               { name: 'Authentications', path: 'authentications_path', condition: 'not PROVIDERS.blank?' },
@@ -31,38 +31,38 @@ Contour.setup do |config|
               { name: 'Logout', path: 'destroy_user_session_path' }]
     },
     # {
-    #   name: 'Frames', id: 'frames', display: 'signed_in', path: 'frames_path', position: 'left',
+    #   name: 'Frames', display: 'signed_in', path: 'frames_path', position: 'left',
     #   links: [{ name: '&raquo;New', path: 'new_frame_path' }]
     # },
     # {
-    #   name: 'Stickies', id: 'stickies', display: 'signed_in', path: 'stickies_path', position: 'left',
+    #   name: 'Stickies', display: 'signed_in', path: 'stickies_path', position: 'left',
     #   links: [{ name: '&raquo;New', path: 'new_sticky_path' },
     #           { name: '&raquo;Graphs', path: 'graph_user_path(current_user.id)' }]
     # },
     {
-      name: 'About', id: 'about', display: 'not_signed_in', path: 'about_path', position: 'left',
+      name: 'About', display: 'not_signed_in', path: 'about_path', position: 'left',
       links: []
     },
     {
-      name: 'Calendar', id: 'calendar', display: 'signed_in', path: 'calendar_stickies_path', position: 'left',
+      name: 'Calendar', display: 'signed_in', path: 'calendar_stickies_path', position: 'left',
       links: [{ name: 'Templates', path: 'templates_path' },
               { html: "<br />" },
               { name: 'About', path: 'about_path' }]
     },
     # {
-    #   name: 'Projects', id: 'projects', display: 'signed_in', path: 'projects_path', position: 'left',
+    #   name: 'Projects', display: 'signed_in', path: 'projects_path', position: 'left',
     #   links: [{ name: '&raquo;New', path: 'new_project_path' }]
     # },
     # {
-    #   name: 'Templates', id: 'templates', display: 'signed_in', path: 'templates_path', position: 'left',
+    #   name: 'Templates', display: 'signed_in', path: 'templates_path', position: 'left',
     #   links: [{ name: '&raquo;New', path: 'new_template_path' }]
     # },
     # {
-    #   name: 'Comments', id: 'comments', display: 'signed_in', path: 'comments_path', position: 'left',
+    #   name: 'Comments', display: 'signed_in', path: 'comments_path', position: 'left',
     #   links: [{ name: '&raquo;New', path: 'new_comment_path' }]
     # },
     {
-      name: 'Users', id: 'users', display: 'signed_in', name: 'Users', path: 'users_path', position: 'left', condition: 'current_user.system_admin?',
+      name: 'Users', display: 'signed_in', name: 'Users', path: 'users_path', position: 'left', condition: 'current_user.system_admin?',
       links: [{ name: 'Overall Graph', path: 'overall_graph_users_path', condition: 'current_user.system_admin?' }]
     }
   ]
