002acaacb602818a240c38fd306dedaa3a2000cb
Remo Mueller <remosm@gmail.com>
2012-01-23 13:54:16 -0500

    Environment files updated to be in sync with Rails 3.2.0

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -2,9 +2,9 @@ Notes::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's
-  # test suite.  You never need to work with it otherwise.  Remember that
+  # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
-  # and recreated between test runs.  Don't rely on the data there!
+  # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
   # Configure static asset server for tests with Cache-Control for performance
@@ -29,7 +29,7 @@ Notes::Application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # # Raise exception on mass assignment protection for ActiveRecord models
+  # Raise exception on mass assignment protection for Active Record models
   # config.active_record.mass_assignment_sanitizer = :strict
 
   # Print deprecation notices to the stderr
