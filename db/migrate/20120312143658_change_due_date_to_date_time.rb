d875c08e2d06ed9aebda2dc995b0849b534af7a7
Remo Mueller <remosm@gmail.com>
2012-03-12 14:47:26 -0400

    Migration modified (still bugged though)

--- db/migrate/20120312143658_change_due_date_to_date_time.rb
+++ db/migrate/20120312143658_change_due_date_to_date_time.rb
@@ -1,6 +1,7 @@
 class ChangeDueDateToDateTime < ActiveRecord::Migration
   def up
     change_column :stickies, :due_date, :datetime
+    Sticky.all.select{|s| not s.due_at.blank?}.each{|s| s.update_attributes(due_date: Time.parse(s.due_date.strftime("%Y-%m-%d ") + s.due_at.strftime("%l:%M %p")), all_day: false ) }
   end
 
   def down
