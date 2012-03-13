7d80417d68aa076899d5b12ebe5b906ebee658c8
Remo Mueller <remosm@gmail.com>
2012-03-13 10:44:24 -0400

    Old Due At time moved to description

--- db/migrate/20120312143658_change_due_date_to_date_time.rb
+++ db/migrate/20120312143658_change_due_date_to_date_time.rb
@@ -1,7 +1,7 @@
 class ChangeDueDateToDateTime < ActiveRecord::Migration
   def up
     change_column :stickies, :due_date, :datetime
-    Sticky.all.select{|s| not s.due_at.blank?}.each{|s| s.update_attributes(due_date: Time.parse(s.due_date.strftime("%Y-%m-%d ") + s.due_at.strftime("%l:%M %p")), all_day: false ) }
+    Sticky.all.select{|s| not s.due_at.blank?}.each{ |s| s.update_attributes(description: s.description + "\n\nDUE AT: " + s.due_at.localtime.strftime("%l:%M %p").strip) }
   end
 
   def down
