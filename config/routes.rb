b5ecfcebaa323368b4ec6531e4016c5f971a904d
Remo Mueller <remosm@gmail.com>
2012-05-04 13:12:25 -0400

    Use Contour for User account Confirmations and Unlocks

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Notes::Application.routes.draw do
     # end
   end
 
-  devise_for :users, controllers: { registrations: 'contour/registrations', sessions: 'contour/sessions', passwords: 'contour/passwords' }, path_names: { sign_up: 'register', sign_in: 'login' }
+  devise_for :users, controllers: { registrations: 'contour/registrations', sessions: 'contour/sessions', passwords: 'contour/passwords', confirmations: 'contour/confirmations', unlocks: 'contour/unlocks' }, path_names: { sign_up: 'register', sign_in: 'login' }
 
   resources :users do
     collection do
