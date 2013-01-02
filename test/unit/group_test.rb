eb0231541a401d770655202bc5b7fdedaeab8a5f
Remo Mueller <remosm@gmail.com>
2013-01-02 14:39:42 -0500

    Tests to check if group descriptions get splits correctly

--- test/unit/group_test.rb
+++ test/unit/group_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get second half of group description" do
+    assert "Second Part", groups(:four).short_description_second_half
+  end
 end
