a70af5d77027d78fb5e03d73ed160430045a7768
Remo Mueller <remosm@gmail.com>
2013-01-18 16:41:21 -0500

    Remove Unused column password_salt from users table

---
+++ db/migrate/20130118213753_remove_password_salt_from_users.rb
@@ -0,0 +1,9 @@
+class RemovePasswordSaltFromUsers < ActiveRecord::Migration
+  def up
+    remove_column :users, :password_salt
+  end
+
+  def down
+    add_column :users, :password_salt, :string, default: '', null: false
+  end
+end
