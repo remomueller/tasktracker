9ac705e724f06ba778297eb147b1eb61f29d4625
Remo Mueller <remosm@gmail.com>
2013-02-04 10:50:42 -0500

    Added tests for about page and search page

---
+++ test/functional/application_controller_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+
+class ApplicationControllerTest < ActionController::TestCase
+  setup do
+    # Nothing
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get search" do
+    login(users(:valid))
+    get :search, q: ''
+
+    assert_not_nil assigns(:projects)
+    assert_not_nil assigns(:objects)
+
+    assert_response :success
+  end
+
+  test "should get search and redirect" do
+    login(users(:valid))
+    get :search, q: "Ongoing Valid's Project"
+
+    assert_not_nil assigns(:projects)
+    assert_not_nil assigns(:objects)
+
+    assert_equal 1, assigns(:objects).size
+
+    assert_redirected_to assigns(:objects).first
+  end
+
+  test "should get search typeahead" do
+    login(users(:valid))
+    get :search, q: 'abc', format: 'json'
+
+    assert_not_nil assigns(:projects)
+    assert_not_nil assigns(:objects)
+
+    assert_response :success
+  end
+
+end
