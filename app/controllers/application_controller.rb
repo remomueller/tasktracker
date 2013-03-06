73356d50b9fcdf21445650a3085dfc1734df2077
Remo Mueller <remosm@gmail.com>
2013-03-06 12:01:43 -0500

    Added setting viewable and editable project methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -56,4 +56,25 @@ class ApplicationController < ActionController::Base
     order = column_name.blank? ? default_order : [column_name, direction].compact.join(' ')
     order
   end
+
+  def set_viewable_project(id = :project_id)
+    @project = current_user.all_viewable_projects.find_by_id(params[id])
+  end
+
+  def set_editable_project(id = :project_id)
+    @project = current_user.all_projects.find_by_id(params[id])
+  end
+
+  def redirect_without_project(path = root_path)
+    empty_response_or_root_path(path) unless @project
+  end
+
+  def empty_response_or_root_path(path = root_path)
+    respond_to do |format|
+      format.html { redirect_to path }
+      format.js { render nothing: true }
+      format.json { head :no_content }
+    end
+  end
+
 end
