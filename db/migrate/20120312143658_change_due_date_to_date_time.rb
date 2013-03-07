c3a2a2d31e4a4377fa805cfad5577a081b5b02df
Remo Mueller <remosm@gmail.com>
2013-03-07 12:06:27 -0500

    Removed localtime conversions

--- db/migrate/20120312143658_change_due_date_to_date_time.rb
+++ db/migrate/20120312143658_change_due_date_to_date_time.rb
@@ -2,7 +2,7 @@ class ChangeDueDateToDateTime < ActiveRecord::Migration
   def up
     change_column :stickies, :due_date, :datetime
     Sticky.all.select{|s| not s.due_date.blank?}.each{|s| s.update_attributes(due_date: s.due_date.at_midnight + 1.day) }
-    Sticky.all.select{|s| not s.due_date.blank? and not s.read_attribute('due_at').blank?}.each{ |s| s.update_attributes(all_day: false, due_date: s.due_date + s.read_attribute('due_at').localtime.strftime("%H").to_i.hours + s.read_attribute('due_at').localtime.strftime("%M").to_i.minutes) }
+    Sticky.all.select{|s| not s.due_date.blank? and not s.read_attribute('due_at').blank?}.each{ |s| s.update_attributes(all_day: false, due_date: s.due_date + s.read_attribute('due_at').strftime("%H").to_i.hours + s.read_attribute('due_at').localtime.strftime("%M").to_i.minutes) }
   end
 
   def down
