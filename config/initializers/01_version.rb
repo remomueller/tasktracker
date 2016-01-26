f95f5c9053014cd422abf780b912daa4a37e3470
Remo Mueller <remosm@gmail.com>
2016-01-26 14:39:27 -0500

    Version bump to 0.28.12.pre

--- config/initializers/01_version.rb
+++ config/initializers/01_version.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 module TaskTracker
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 28
-    TINY = 11
-    BUILD = nil # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
+    TINY = 12
+    BUILD = 'pre'.freeze # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
 
-    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
+    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.').freeze
   end
 end
