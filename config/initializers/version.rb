99faca8e3153b7ac25234511c77df607a5351b0c
Remo Mueller <remosm@gmail.com>
2016-07-25 11:52:47 -0400

    Version bump to 0.30.0.pre

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -3,9 +3,9 @@
 module TaskTracker
   module VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 29
+    MINOR = 30
     TINY = 0
-    BUILD = nil # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
+    BUILD = 'pre' # 'pre', 'beta1', 'beta2', 'rc', 'rc2', nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.').freeze
   end
