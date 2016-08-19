436fbe3b905658b46f7bf4aa38e8955c7dea1705
Remo Mueller <remosm@gmail.com>
2016-08-19 10:41:34 -0400

    Emails are enabled if a user opts in to emails during sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
       :sign_up,
-      keys: [:first_name, :last_name, :email, :password, :password_confirmation] # , :emails_enabled
+      keys: [:first_name, :last_name, :email, :password, :password_confirmation, :emails_enabled]
     )
   end
 
